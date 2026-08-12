a77e121d280dbe21a70fa0e8ff869568a007bcaa
sriramsme <123972028+sriramsme@users.noreply.github.com>
2026-08-12 14:32:14 -0500

    feat: system theme toggle Super+Shift+T via gsettings

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -341,6 +341,11 @@ hl.bind(shiftSuperMod .. " + B", hl.dsp.exec_cmd("blueman-manager"))
 
 
 hl.bind(mainMod .. " + O", hl.dsp.exec_cmd("handy --toggle-transcription"))
+
+hl.bind("SUPER + SHIFT + T",
+    hl.dsp.exec_cmd(
+        "bash -c 'if [ \"$(gsettings get org.gnome.desktop.interface color-scheme)\" = \"'\\''prefer-dark'\\''\" ]; then gsettings set org.gnome.desktop.interface color-scheme prefer-light; else gsettings set org.gnome.desktop.interface color-scheme prefer-dark; fi'"))
+
 ---- WINDOWS AND WORKSPACES ----
 --------------------------------
 
